8b51ff7c7a3c08d8d44bacb27d482b20b4935581
JasKay <iamjasanyakay@gmail.com>
2020-07-07 10:57:59 -0400

    save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App"
 
+ReactDOM.render(
+  <div>
+  <App />
+  <h1>Hello WOrld</h1>
+  </div>,
+  document.getElementById("root")
+);
