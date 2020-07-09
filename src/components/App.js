d61ac37f5ebd239a1d5e0c8f5baf30a79378066e
JasKay <iamjasanyakay@gmail.com>
2020-07-08 21:47:05 -0400

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import Header from './Header'
-import Footer from './Footer'
-import Note from './Note'
-import './styles.css'
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import Card from "./Card";
+import contacts from "../Contacts";
+
+import './styles.css';
 
 function App() {
   return <div>
     <Header />
     <Note />
     <Footer />
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Edoyak</h2>
-          <img className="circle"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIDTRdQ0s_REO_PS1drsrdbzKcSRP2GOkgZA&usqp=CAU" alt="kay-img"></img>
-        </div>
-        <div className="bottom">
-          <p>+1-234-567-890</p>
-          <p>kay@kay.com</p>
-        </div>
-      </div>
-    </div>
+    <Card
+      name = {contacts[0].myname}
+      img  = {contacts[0].myimg}
+      tel  = {contacts[0].mytel}
+      email= {contacts[0].myemail}
+      />
+    <Card
+      name = {contacts[1].myname}
+      img  = {contacts[1].myimg}
+      tel  = {contacts[1].mytel}
+      email= {contacts[1].myemail}
+      />
+      <Card
+      name = {contacts[2].myname}
+      img  = {contacts[2].myimg}
+      tel  = {contacts[2].mytel}
+      email= {contacts[2].myemail}
+      />
+      <Card
+      name = {contacts[3].myname}
+      img  = {contacts[3].myimg}
+      tel  = {contacts[3].mytel}
+      email= {contacts[3].myemail}
+      />
   </div>
 }
 
---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+    {
+        myname : "Boy Man", 
+        myimg  : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIDTRdQ0s_REO_PS1drsrdbzKcSRP2GOkgZA&usqp=CAU",
+        mytel  : "+1-234-567-890",
+        myemail: "iamjasanyakay@gmail.com"
+    },
+    {
+        myname : "Jack Bauer" ,
+        myimg  : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIDTRdQ0s_REO_PS1drsrdbzKcSRP2GOkgZA&usqp=CAU",
+        mytel  : "+2-345-678-901",
+        myemail: "iamjasanyakay@gmail.com"
+    },
+    {
+        myname : "Scalentino", 
+        myimg  : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIDTRdQ0s_REO_PS1drsrdbzKcSRP2GOkgZA&usqp=CAU",
+        mytel  : "+3-456-789-012",
+        myemail: "iamjasanyakay@gmail.com"
+    },
+    {
+        myname : "Grapefruit", 
+        myimg  : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIDTRdQ0s_REO_PS1drsrdbzKcSRP2GOkgZA&usqp=CAU",
+        mytel  : "+1-234-567-890",
+        myemail: "iamjasanyakay@gmail.com"
+    },
+];
+
+export default contacts;
