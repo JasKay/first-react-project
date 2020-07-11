f1f01d52b2e0a593dd92b59b8277496737f6c863
JasKay <iamjasanyakay@gmail.com>
2020-07-11 18:38:23 -0400

    save

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Avatar(props){
-    return <img className="circle-img"  src={props.img} alt="kay-imgs"></img>
+    return <img className="circle-img"  src={props.img} alt="kay-imggs"></img>
 }
 
 export default Avatar;
