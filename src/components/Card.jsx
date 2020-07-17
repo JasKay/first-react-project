2406bfa096339e901128357baed0fc805cae4c52
JasKay <iamjasanyakay@gmail.com>
2020-07-17 09:53:55 -0400

    save

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import Detail from "./Detail";
 function Card(props){
 return (
     <div>
-      <h1 className="heading">My Contacts</h1>
+      <h1 className="heading">My Contacts for Today</h1>
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
